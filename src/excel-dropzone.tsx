cf6a94dd55b56c687e155c3eccb90246959afe83
Ljubomir Bojadziev <ljubomir.bojadziev@gmail.com>
2025-01-31 15:55:50 +0100

    Refactor excel dropzone

--- src/excel-dropzone.tsx
+++ src/excel-dropzone.tsx
@@ -1,49 +1,38 @@
-import React from 'react'
-import Dropzone from 'react-dropzone'
-import { Box, Center } from '@northlight/ui'
-import { utils, read } from 'xlsx'
-import { palette } from '@northlight/tokens'
+import { palette } from "@northlight/tokens";
+import { Box, Center } from "@northlight/ui";
+import React from "react";
+import Dropzone from "react-dropzone";
+import { read, utils } from "xlsx";
 
-export interface ExcelRow {
-  name: string
-  score: number
-}
+import type { ExcelDropzoneProps } from "./types/types";
 
-export interface ExcelDropzoneProps {
-  onSheetDrop: (rows: ExcelRow[]) => void
-  label: string
-}
+export function ExcelDropzone(props: ExcelDropzoneProps) {
+	const { label, onSheetDrop } = props;
 
-export function ExcelDropzone (props: ExcelDropzoneProps) {
-  const { label, onSheetDrop } = props
+	const handleFile = (acceptedFiles: File[]) => {
+		const file = acceptedFiles[0];
+		const reader = new window.FileReader();
+		reader.onload = (e) => {
+			const data = e.target?.result;
+			const workbook = read(data, { type: "binary" });
+			const sheetName = workbook.SheetNames[0];
+			onSheetDrop(utils.sheet_to_json(workbook.Sheets[sheetName]));
+		};
+		reader.readAsBinaryString(file);
+	};
 
-  function handleFile (acceptedFiles: File[]) {
-    const file = acceptedFiles[0]
-    const reader = new window.FileReader()
-    reader.onload = function (e) {
-      const data = e.target?.result
-      const workbook = read(data, { type: 'binary' })
-      const sheetName = workbook.SheetNames[0]
-      onSheetDrop(utils.sheet_to_json(workbook.Sheets[sheetName]))
-    }
-    reader.readAsBinaryString(file)
-  }
-
-  return (
-      <Dropzone
-        multiple={ false }
-        onDrop={ handleFile }
-      >
-        {({getRootProps}) => (
-          <Box
-            { ...getRootProps() }
-            border="2px dashed"
-            borderColor={ palette.gray['200']}
-            minW="20%"
-          >
-            <Center height="200">{ label }</Center>
-          </Box>
-      )}
-    </Dropzone>
-  )
+	return (
+		<Dropzone multiple={false} onDrop={handleFile}>
+			{({ getRootProps }) => (
+				<Box
+					{...getRootProps()}
+					border="2px dashed"
+					borderColor={palette.gray["200"]}
+					minW="20%"
+				>
+					<Center height="200">{label}</Center>
+				</Box>
+			)}
+		</Dropzone>
+	);
 }
