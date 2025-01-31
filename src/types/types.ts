7fd0a65956f925b7530c8157af0de63f746ed386
Ljubomir Bojadziev <ljubomir.bojadziev@gmail.com>
2025-01-31 15:45:39 +0100

    Move exported types to a separate folder and file

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export type UserScore = {
+	name: string;
+	score: number;
+	userId: number;
+};
+
+export type ExcelRow = {
+	name: string;
+	score: number;
+};
+
+export type ExcelDropzoneProps = {
+	label: string;
+	onSheetDrop: (rows: ExcelRow[]) => void;
+};
