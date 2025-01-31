b4c50af7f8cfb3c294a27bfc0d8b6d84b228b5b4
Ljubomir Bojadziev <ljubomir.bojadziev@gmail.com>
2025-01-31 17:31:33 +0100

    Create constants file and implement toaster

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,26 @@
-import { Container, H1 } from "@northlight/ui";
+import { Container, H1, useToast } from "@northlight/ui";
 import React, { useState } from "react";
 
 import { AddUserScoreSection } from "./components/AddUserScoreSection.js";
-import { ExerciseDescription } from "./components/ExerciseDescription.js";
+// import { ExerciseDescription } from "./components/ExerciseDescription.js";
 import { UserScoresTable } from "./components/UserScoresTable.js";
+import { TOAST_SETTINGS } from "./constants";
 import type { ExcelRow } from "./types/types";
 import { returnImportedUserScores } from "./utils/returnImportedUserScores.js";
 import { returnInitialUserScores } from "./utils/returnInitialUserScores.js";
 
 export default function App() {
+	const toast = useToast();
 	const [userScores, setUserScores] = useState(returnInitialUserScores());
 
 	const handleSheetData = (data: ExcelRow[]) => {
 		const updatedUserScores = returnImportedUserScores(userScores, data);
 		setUserScores(updatedUserScores);
+		// @ts-expect-error
+		toast({
+			description: "The excel file has been imported successfully",
+			...TOAST_SETTINGS,
+		});
 	};
 
 	const handleAddNewScore = (newUser: { name: string; score: number }) => {
@@ -22,12 +29,17 @@ export default function App() {
 			{ ...newUser, userId: userScores.length + 1 },
 		];
 		setUserScores(updatedUserScores.sort((a, b) => b.score - a.score));
+		// @ts-expect-error
+		toast({
+			description: "The user score has been added successfully",
+			...TOAST_SETTINGS,
+		});
 	};
 
 	return (
 		<Container maxW="6xl" padding="4">
 			<H1 marginBottom="4">Mediatool exercise</H1>
-			<ExerciseDescription />
+			{/* <ExerciseDescription /> */}
 			<AddUserScoreSection
 				handleSheetData={handleSheetData}
 				handleAddNewScore={handleAddNewScore}
---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const TOAST_SETTINGS = {
+	title: " ",
+	duration: 3000,
+	isClosable: true,
+	variant: "left-accent",
+	position: "bottom-left",
+};
