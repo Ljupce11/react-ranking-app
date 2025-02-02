74faf55c082fec7842b697d0e205ea020551a662
Ljubomir Bojadziev <ljubomir.bojadziev@gmail.com>
2025-02-02 16:17:36 +0100

    Update toast settings to use success variant and type for position

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
+import type { ToastPosition } from "@chakra-ui/react";
+
 export const TOAST_SETTINGS = {
 	title: " ",
 	duration: 3000,
 	isClosable: true,
-	variant: "left-accent",
-	position: "bottom-left",
+	variant: "success",
+	position: "bottom-left" as ToastPosition,
 };
